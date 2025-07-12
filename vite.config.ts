0ec3c079340213c3bb302091f066d24b6831eda8
Bilal Qureshi <bilalqureshi1390@gmail.com>
2025-07-12 21:35:30 +0500

    fix: update vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// export default defineConfig({
+//   base: "/bilal_qureshi_portfolio/",
+//   plugins: [react()],
+// });
+
+// vite.config.js
 export default defineConfig({
   base: "/bilal_qureshi_portfolio/",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // optional: to silence warning
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
